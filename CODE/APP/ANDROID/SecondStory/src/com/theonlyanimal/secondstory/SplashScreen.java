879aca03ada40d7c88662b335617c387ba9654e9
Jesse Scott <jessecolinscott@gmail.com>
2014-04-06 17:55:20 -0700

    Threaded Timer

---
+++ CODE/APP/ANDROID/SecondStory/src/com/theonlyanimal/secondstory/SplashScreen.java
@@ -0,0 +1,45 @@
+package com.theonlyanimal.secondstory;
+
+// IMPORTS
+import android.os.Bundle;
+import android.util.Log;
+import android.app.Activity;
+import android.content.Intent;
+
+
+// CLASS
+public class SplashScreen extends Activity {
+
+	// GLOBALS
+	private static final String TAG = "SecondStory";
+	
+	// LifeCycle
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+        
+        // Timer
+		Thread timer = new Thread(){
+			@Override
+			public void run() {
+				//super.run();
+				try { 
+					// Time for 5 seconds
+					sleep(5000);
+					Intent i = new Intent("android.intent.action.WELCOME");
+					startActivity(i);
+				}
+				catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+				finally {
+					Log.v(TAG, " - Ending The Initial Screen - ");
+					finish();
+				}
+			}
+		};
+		timer.start();
+    }
+	
+}
