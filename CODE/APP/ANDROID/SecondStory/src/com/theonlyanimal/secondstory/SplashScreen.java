27a4f7f735986409de93b32083bebb6bfef71436
Jesse Scott <jessecolinscott@gmail.com>
2014-04-06 17:42:50 -0700

    Deleted Unused Window Flags

--- CODE/APP/ANDROID/SecondStory/src/com/theonlyanimal/secondstory/SplashScreen.java
+++ CODE/APP/ANDROID/SecondStory/src/com/theonlyanimal/secondstory/SplashScreen.java
@@ -2,8 +2,6 @@ package com.theonlyanimal.secondstory;
 
 // IMPORTS
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 import android.app.Activity;
 
 
@@ -15,8 +13,6 @@ public class SplashScreen extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE); 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash_layout);
     }
 	
