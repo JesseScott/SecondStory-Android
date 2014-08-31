153416bc46f5e9777cdb62562dcff7430bb1a0ff
Jesse Scott <jessecolinscott@gmail.com>
2014-08-30 20:04:06 -0700

    buttons! click listneners!

---
+++ SecondStory/src/com/theonlyanimal/secondstory/MenuScreen.java
@@ -0,0 +1,80 @@
+package com.theonlyanimal.secondstory;
+
+
+//IMPORTS
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.app.Activity;
+import android.content.Intent;
+
+
+
+//CLASS
+public class MenuScreen extends Activity {
+
+	// GLOBALS
+	private static final String TAG = "SS_MENU";
+
+	
+	// LifeCycle
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
+		setContentView(R.layout.menu_layout);
+		
+		Button guideBtn = (Button) findViewById(R.id.menu_guide_btn);
+		guideBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent("android.intent.action.GUIDE");
+	    		startActivity(i);
+			}
+		});
+		
+		Button mapsBtn = (Button) findViewById(R.id.menu_maps_btn);
+		mapsBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent("android.intent.action.MAPS");
+	    		startActivity(i);
+			}
+		});
+		
+		Button settingsBtn = (Button) findViewById(R.id.menu_settings_btn);
+		settingsBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent("android.intent.action.SETTINGS");
+	    		startActivity(i);
+			}
+		});
+		
+		Button aboutBtn = (Button) findViewById(R.id.menu_about_btn);
+		aboutBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent("android.intent.action.ABOUT");
+	    		startActivity(i);
+			}
+		});
+		
+		Button liveBtn = (Button) findViewById(R.id.menu_live_btn);
+		liveBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent("android.intent.action.LIVE");
+	    		startActivity(i);
+	    		finish();
+			}
+		});
+
+
+	}
+ 
+
+ 
+ 
+} /* EOC */
