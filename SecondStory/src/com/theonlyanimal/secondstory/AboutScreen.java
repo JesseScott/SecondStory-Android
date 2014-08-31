6fdc2a6b982d86e3991318b1ec53df4fc774de70
Jesse Scott <jessecolinscott@gmail.com>
2014-08-31 01:01:31 -0700

    Instance ListView !

--- SecondStory/src/com/theonlyanimal/secondstory/AboutScreen.java
+++ SecondStory/src/com/theonlyanimal/secondstory/AboutScreen.java
@@ -5,13 +5,22 @@ package com.theonlyanimal.secondstory;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 
 //CLASS
 public class AboutScreen extends Activity {
 
 	// GLOBALS
 	private static final String TAG = "SS_GUIDE";
+	private static Context context;
+	String[] instances;
 
 	
 	// LifeCycle
@@ -20,6 +29,22 @@ public class AboutScreen extends Activity {
 		super.onCreate(savedInstanceState);
 		//overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
 		setContentView(R.layout.about_layout);
+		context = this;
+		
+		// ListView
+		ListView locations = (ListView) findViewById(R.id.instance_listview);
+		instances = getResources().getStringArray(R.array.instances_array);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, R.layout.listview_item, R.id.listview_item_label, instances);
+		locations.setAdapter(adapter);
+		locations.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				//Toast.makeText(context, "Row " + position + " selected", Toast.LENGTH_SHORT).show();       
+				Intent i = new Intent("android.intent.action.TUTORIAL");
+				i.putExtra("instance", position);
+				startActivity(i);
+			};
+		});
 
 	}
  
