6063c519cb5dea48c79f874ff7498dff85005786
Jesse Scott <jessecolinscott@gmail.com>
2014-08-30 23:12:34 -0700

    new screens (onwards!!)

---
+++ SecondStory/src/com/theonlyanimal/secondstory/SettingsScreen.java
@@ -0,0 +1,29 @@
+package com.theonlyanimal.secondstory;
+
+
+//IMPORTS
+
+import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
+
+//CLASS
+public class SettingsScreen extends Activity {
+
+	// GLOBALS
+	private static final String TAG = "SS_GUIDE";
+
+	
+	// LifeCycle
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
+		setContentView(R.layout.settings_layout);
+
+	}
+ 
+
+ 
+ 
+} /* EOC */
