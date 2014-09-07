30d14bb2b16b54b28d1a98f2a2a737f54866044b
Jesse Scott <jessecolinscott@gmail.com>
2014-09-06 19:05:52 -0700

    Splash Layout updated

--- SecondStory/src/com/theonlyanimal/secondstory/SplashScreen.java
+++ SecondStory/src/com/theonlyanimal/secondstory/SplashScreen.java
@@ -3,7 +3,6 @@ package com.theonlyanimal.secondstory;
 // IMPORTS
 
 import android.os.Bundle;
-import android.util.Log;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
