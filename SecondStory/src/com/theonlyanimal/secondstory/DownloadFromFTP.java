5a6eb82ae1e112e4545e9e4189e33fbf93aef2ba
Jesse Scott <jessecolinscott@gmail.com>
2014-09-23 01:47:45 -0700

    testing ftp service

---
+++ SecondStory/src/com/theonlyanimal/secondstory/DownloadFromFTP.java
@@ -0,0 +1,116 @@
+package com.theonlyanimal.secondstory;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.OutputStream;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+
+import android.text.format.Time;
+import android.util.Log;
+
+public class DownloadFromFTP {
+	
+	private static final String MEDIA_DIRECTORY = "//sdcard//SecondStory/BloodAlley/MEDIA/";
+	private static final String LOG_DIRECTORY = "//sdcard//SecondStory/BloodAlley/LOGS/";
+	
+	public boolean ftpDownload(String srcFilePath,String desFileName) {
+		
+		boolean status = false;
+		final String username = "ghostlight"; 
+		final String password = "gh0st_l1ght"; 
+		final String hostname = "ftp.memelab.ca"; 
+		final int port = 21; 
+		
+		String SFTPWORKINGDIR = "/public_html/jessescott/storage/android";
+		
+		FTPClient ftp = new FTPClient();
+		try {
+			ftp.connect(hostname, port);
+			ftp.login(username, password);
+			
+			ftp.changeWorkingDirectory(SFTPWORKINGDIR);
+			ftp.setFileType(FTP.BINARY_FILE_TYPE);
+			ftp.setBufferSize(1024);
+			ftp.enterLocalPassiveMode();
+			
+			OutputStream output = null;
+			FTPFile[] files = ftp.listFiles();
+			
+			// Set Log Directory & File
+			File logFile = new File(LOG_DIRECTORY + "logfile.txt");
+			Log.v("FTP", "LOG should exist at " + logFile.getAbsolutePath());
+			if(!logFile.exists()) {
+				try {
+					logFile.createNewFile();
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+			
+			for (FTPFile f : files) {
+				// Log Names
+				Log.v("FTP", f.toFormattedString());
+				
+				// Set Path
+				String remoteFile = f.getName();
+				Log.v("FTP", "REMOTE file " + remoteFile);
+				String localFile = MEDIA_DIRECTORY;
+				localFile += remoteFile;
+				Log.v("FTP", " is being put in LOCAL path " + localFile);
+			    output = new BufferedOutputStream(new FileOutputStream(localFile));
+			    
+				// Set Time
+			    Time now = new Time(Time.getCurrentTimezone());
+			    now.setToNow();
+				
+				// Set Logger
+			    BufferedWriter logger = new BufferedWriter(new FileWriter(logFile, true)); 
+			    logger.append("Download for " + localFile + " started at " + now.format("%k:%M:%S"));
+			    logger.newLine();
+			    Long startTime = System.currentTimeMillis();
+			    
+			    // HTTP
+			    System.setProperty("http.keepAlive", "false");
+
+			    // Get Files
+                Boolean success = ftp.retrieveFile(remoteFile, output);
+                status = success;
+                if(success) {
+                	Log.v("FTP", "SUCCESS");
+                	now.setToNow();
+                	Long elapsedTime = (System.currentTimeMillis() - startTime) / 1000;
+				    logger.append("Download for " + localFile + " finished at " + now.format("%k:%M:%S"));
+				    logger.newLine();
+				    logger.append("for an elapsedTime of " + elapsedTime + " seconds");
+				    logger.newLine();
+				    logger.newLine();
+                }
+                
+                // Close Logger
+                logger.flush();
+                logger.close();
+                
+                // Close Buffer 
+                if(ftp != null) {
+					output.close();
+				}
+			 }
+
+			ftp.logout();
+			ftp.disconnect();
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return status;
+	}
+
+
+}
---
+++ SecondStory/src/com/theonlyanimal/secondstory/FTPService.java
@@ -0,0 +1,92 @@
+package com.theonlyanimal.secondstory;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.Environment;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+public class FTPService extends Service {
+	
+	private MyThread mythread;
+	public boolean isRunning = false;
+	long interval = 3000;
+	
+	// Defines a custom Intent action
+    public static final String BROADCAST_ACTION = "com.theonlyanimal.secondstory.FTPService.BROADCAST";
+    public static final String EXTENDED_DATA_STATUS = "com.theonlyanimal.secondstory.FTPService.STATUS";
+	
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return null;
+	}
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		mythread = new MyThread(interval);
+		String status = "";
+		Intent localIntent = new Intent(BROADCAST_ACTION).putExtra(EXTENDED_DATA_STATUS, status);
+	    LocalBroadcastManager.getInstance(this).sendBroadcast(localIntent);
+	}
+	
+	@Override
+	public synchronized void onDestroy() {
+		super.onDestroy();
+		if(!isRunning){
+			mythread.interrupt();
+			//myythread.stop();
+			mythread = null;
+		}
+	}
+	
+	@Override
+	public synchronized int onStartCommand(Intent intent, int flags, int startId) {
+		super.onStartCommand(intent, flags, startId);
+		if(!isRunning){
+			mythread.start();
+			isRunning = true;
+		}
+		return Service.START_NOT_STICKY;
+	}
+	
+	class MyThread extends Thread {
+		long interval;
+		public MyThread(long interval){
+			this.interval = interval;
+		}
+		
+		@Override
+		public void run(){
+			while(isRunning){
+				System.out.println("Service running");
+				try {
+					fileDownload();
+					Thread.sleep(interval);
+				} catch (InterruptedException e) {
+					isRunning = false;
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		private void fileDownload() {
+			if(checkInternetConnection()){
+				boolean get = new DownloadFromFTP().ftpDownload("", "");
+				Log.d("SS", "getting ?  " + get);
+			}
+		}
+	}
+	private boolean checkInternetConnection() {
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isAvailable() && cm.getActiveNetworkInfo().isConnected()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
