feb911a2b38319ac76779f183273c47a3f240122
Jesse Scott <jessecolinscott@gmail.com>
2014-09-23 19:54:24 -0700

    youtube playback selection

--- SecondStory/src/com/theonlyanimal/ar/FullscreenYoutubePlayback.java
+++ SecondStory/src/com/theonlyanimal/ar/FullscreenYoutubePlayback.java
@@ -1,6 +1,7 @@
 package com.theonlyanimal.ar;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
@@ -15,10 +16,22 @@ import com.theonlyanimal.secondstory.R;
 
 public class FullscreenYoutubePlayback extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
 	
-	public static final String API_KEY = "AIzaSyB-AuNZSDst3VMYKF5CTm4JZfXgLQ5eCq0";
+	public static final String API_KEY = "AIzaSyC6yA82mS3VWCWXZQZ_zgiSC_lItLukSfY";
 
 	//http://youtu.be/<VIDEO_ID>
-	public static final String VIDEO_ID = "N0L1xyy8tqA";
+	private String mMovieName = "";
+	public static String VIDEO_ID_PLAYABLE = "";
+	
+	public static final String VIDEO_ID_BEEF = "N0L1xyy8tqA";
+	public static final String VIDEO_ID_BICYCLES = "5npVxU_FMxg";
+	public static final String VIDEO_ID_ALLEY = "zHUf_d3tUM";
+	public static final String VIDEO_ID_COPPER = "ZaM6fWMAu-Q";
+	public static final String VIDEO_ID_GUN = "VqKuwHpkI4o";
+	public static final String VIDEO_ID_PENNIES = "UA_T7eHy8mM";
+	public static final String VIDEO_ID_SHROOMS = "YDkUmpZYcXc";
+	public static final String VIDEO_ID_SWEEPING = "F4BiLpAxFTs";
+	public static final String VIDEO_ID_UMBRELLAS = "vwLm44Og9xU";
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +39,11 @@ public class FullscreenYoutubePlayback extends YouTubeBaseActivity implements Yo
 
 		/** attaching layout xml **/
 		setContentView(R.layout.fullscreen_youtube_layout);
+		
+		/** Getting Movie Name **/
+		mMovieName = getIntent().getStringExtra("movieName");
+		Log.v("SS YT", "Name is " + mMovieName);
+		determineVideoID(mMovieName);
 
 		/** Initializing YouTube player view **/
 		YouTubePlayerView youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_player);
@@ -34,10 +52,8 @@ public class FullscreenYoutubePlayback extends YouTubeBaseActivity implements Yo
 	}
 	
 	@Override
-	public void onInitializationFailure(Provider arg0,
-			YouTubeInitializationResult arg1) {
+	public void onInitializationFailure(Provider arg0, YouTubeInitializationResult arg1) {
 		Toast.makeText(this, "Failured to Initialize!", Toast.LENGTH_LONG).show();
-		
 	}
 
 	@Override
@@ -45,12 +61,53 @@ public class FullscreenYoutubePlayback extends YouTubeBaseActivity implements Yo
 		/** add listeners to YouTubePlayer instance **/
 		player.setPlayerStateChangeListener(playerStateChangeListener);
 		player.setPlaybackEventListener(playbackEventListener);
-
+		player.setFullscreen(true);
+		
 		/** Start buffering **/
 		if (!wasRestored) {
-			player.cueVideo(VIDEO_ID);
+			player.cueVideo(VIDEO_ID_PLAYABLE);
 		}
 		
+	}
+	
+	private void determineVideoID(String name) {
+		
+		// Trim
+		String delimit = "/storage/emulated/0/SecondStory/BloodAlley/MEDIA/";
+		String trim = name.substring(delimit.length(), name.length() -4);
+		Log.v("SS YT", "Trimmed: " + trim);
+		
+		if(trim.compareTo("beef") == 0) {
+			VIDEO_ID_PLAYABLE = VIDEO_ID_BEEF;
+		}
+		else if(trim.compareTo("bicycles") == 0) {
+			VIDEO_ID_PLAYABLE = VIDEO_ID_BICYCLES;
+		}
+		else if(trim.compareTo("bloodalley") == 0) {
+			VIDEO_ID_PLAYABLE = VIDEO_ID_ALLEY;
+		}
+		else if(trim.compareTo("copperthief") == 0) {
+			VIDEO_ID_PLAYABLE = VIDEO_ID_COPPER;
+		}
+		else if(trim.compareTo("gun") == 0) {
+			VIDEO_ID_PLAYABLE = VIDEO_ID_GUN;
+		}
+		else if(trim.compareTo("pennies") == 0) {
+			VIDEO_ID_PLAYABLE = VIDEO_ID_PENNIES;
+		}
+		else if(trim.compareTo("shrooms") == 0) {
+			VIDEO_ID_PLAYABLE = VIDEO_ID_SHROOMS;
+		}
+		else if(trim.compareTo("sweeping") == 0) {
+			VIDEO_ID_PLAYABLE = VIDEO_ID_SWEEPING;
+		}
+		else if(trim.compareTo("umbrellas") == 0) {
+			VIDEO_ID_PLAYABLE = VIDEO_ID_UMBRELLAS;
+		}
+		else  {
+			VIDEO_ID_PLAYABLE = "hrXwOT765Yk"; // trailer
+		}
+
 	}
 	
 	private PlaybackEventListener playbackEventListener = new PlaybackEventListener() {
