b1038e224e2063d52c47f68d8c1c2402ed48ae6d
JesseScott <jessecolinscott@gmail.com>
2015-09-02 02:13:53 -0700

    (hardcoded) movie playback from gallery

--- SecondStory/app/src/main/java/com/theonlyanimal/secondstory/activities/GalleryScreen.java
+++ SecondStory/app/src/main/java/com/theonlyanimal/secondstory/activities/GalleryScreen.java
@@ -1,59 +1,131 @@
 package com.theonlyanimal.secondstory.activities;
 
 
-import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 
 import com.theonlyanimal.secondstory.R;
-import com.theonlyanimal.secondstory.fragments.GallerySlidePageFragment;
+import com.theonlyanimal.secondstory.ar.FullscreenPlayback;
+import com.theonlyanimal.secondstory.ar.FullscreenYoutubePlayback;
+import com.theonlyanimal.secondstory.fragments.GalleryFragment;
+import com.theonlyanimal.secondstory.helpers.Constants;
+
 
 public class GalleryScreen extends FragmentActivity {
 
-    private static final int NUM_PAGES = 8;
+    private static final int NUM_PAGES = 8; // TODO: get real number (7) (Parse)
     private ViewPager mPager;
     private PagerAdapter mPagerAdapter;
+    Intent intentToPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gallery_layout);
 
-        // Instantiate a ViewPager and a PagerAdapter.
         mPager = (ViewPager) findViewById(R.id.gallery_pager);
         mPagerAdapter = new GalleryPagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPagerAdapter);
-        mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                // When changing pages, reset the action bar actions since they are dependent
-                // on which page is currently active. An alternative approach is to have each
-                // fragment expose actions itself (rather than the activity exposing actions),
-                // but for simplicity, the activity provides the actions in this sample.
-                invalidateOptionsMenu();
-            }
-        });
     }
 
 
-
-
-    private class GalleryPagerAdapter extends FragmentStatePagerAdapter {
+    private class GalleryPagerAdapter extends FragmentStatePagerAdapter implements GalleryFragment.OnMovieSelectedListener {
         public GalleryPagerAdapter(android.support.v4.app.FragmentManager fm) {
             super(fm);
         }
 
         @Override
         public android.support.v4.app.Fragment getItem(int position) {
-            return GallerySlidePageFragment.create(position);
+            GalleryFragment frag = GalleryFragment.create(position);
+            frag.setListener(this);
+            return frag;
         }
 
         @Override
         public int getCount() {
             return NUM_PAGES;
         }
+
+        @Override
+        public void onMovieSelected(int index) {
+            fireVideo(index);
+        }
     }
+
+    public void fireVideo(int which) {
+
+        //check if user wants to stream
+        if(getApplicationContext().getSharedPreferences("com.theonlyanimal.secondstory", Context.MODE_PRIVATE).getBoolean("com.theonlyanimal.secondstory.stream", false)){
+            //lets start youtube video
+            intentToPreview = new Intent(GalleryScreen.this, FullscreenYoutubePlayback.class);
+        }
+        else {
+            //if downloaded check if videos already downloaded
+            if(Constants.downloadedAllVideos){
+                //show from storage
+                intentToPreview = new Intent(GalleryScreen.this, FullscreenPlayback.class);
+            }
+            else{
+                //if not yet downloaded show from youtube
+                intentToPreview = new Intent(GalleryScreen.this, FullscreenYoutubePlayback.class);
+            }
+        }
+
+        intentToPreview.putExtra("movieName", getMovieName(which));
+        intentToPreview.putExtra("shouldPlayImmediately", true);
+        intentToPreview.setAction(android.content.Intent.ACTION_VIEW);
+        GalleryScreen.this.startActivityForResult(intentToPreview, 1);
+        //startActivity(intentToPreview);
+    }
+
+    public void checkEvent() {
+        // TODO: Parse this
+    }
+
+    public String getMovieName(int index /*, String event */) {
+        String mMovieName = "";
+        String MEDIA_PATH = "";
+        String SD_PATH = Environment.getExternalStorageDirectory().getPath();
+        MEDIA_PATH = SD_PATH + "/SecondStory/dareu/media/"; // TODO: getIntent
+
+        switch (index) { // TODO: smart index
+//            case 0: // Beef
+//                mMovieName = MEDIA_PATH + "beef.mp4";
+//                break;
+            case 1: // Pennies
+                mMovieName = MEDIA_PATH + "exhibita.mp4";
+                break;
+            case 2: // Sweeping
+                mMovieName = MEDIA_PATH + "leaving.mp4";
+                break;
+            case 3: // Copper
+                mMovieName = MEDIA_PATH + "letter pt1.mp4";
+                break;
+            case 4: // Shrooms
+                mMovieName = MEDIA_PATH + "letter pt2.mp4";
+                break;
+            case 5: // Umbrellas
+                mMovieName = MEDIA_PATH + "plant.mp4";
+                break;
+            case 6: // Alley
+                mMovieName = MEDIA_PATH + "portability.mp4";
+                break;
+            case 7: // Bicycles
+                mMovieName = MEDIA_PATH + "stall.mp4";
+                break;
+//            case 9: // Gun
+//                mMovieName = MEDIA_PATH + "gun.mp4";
+//                break;
+            default:
+                mMovieName = MEDIA_PATH + "";
+        }
+        return mMovieName;
+    }
+
 }
--- SecondStory/app/src/main/java/com/theonlyanimal/secondstory/fragments/GalleryFragment.java
+++ SecondStory/app/src/main/java/com/theonlyanimal/secondstory/fragments/GalleryFragment.java
@@ -1,34 +1,38 @@
 package com.theonlyanimal.secondstory.fragments;
 
 
+
 import android.content.res.TypedArray;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.theonlyanimal.secondstory.R;
 
 
-public class GallerySlidePageFragment extends Fragment {
+public class GalleryFragment extends Fragment {
 
     public static final String ARG_PAGE = "page";
+    private static OnMovieSelectedListener mListener;
     private int mPageNumber;
 
-    public static GallerySlidePageFragment create(int pageNumber) {
-        GallerySlidePageFragment fragment = new GallerySlidePageFragment();
+    public static GalleryFragment create(int pageNumber) {
+        GalleryFragment fragment = new GalleryFragment();
         Bundle args = new Bundle();
         args.putInt(ARG_PAGE, pageNumber);
         fragment.setArguments(args);
         return fragment;
     }
 
-    public GallerySlidePageFragment() {
+    public GalleryFragment() {
 
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,7 +48,15 @@ public class GallerySlidePageFragment extends Fragment {
         ((TextView) rootView.findViewById(R.id.gallery_page_director)).setText(getResources().getStringArray(R.array.vid_directors)[mPageNumber]);
 
         TypedArray imgs = getResources().obtainTypedArray(R.array.vid_thumbs);
-        ((ImageView)rootView.findViewById(R.id.gallery_page_bg)).setImageResource(imgs.getResourceId(mPageNumber, -1));
+        ((ImageView) rootView.findViewById(R.id.gallery_page_bg)).setImageResource(imgs.getResourceId(mPageNumber, -1));
+
+        ImageButton play = (ImageButton) rootView.findViewById(R.id.gallery_page_btn);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onMovieSelected(mPageNumber);
+            }
+        });
 
         return rootView;
     }
@@ -52,4 +64,16 @@ public class GallerySlidePageFragment extends Fragment {
     public int getPageNumber() {
         return mPageNumber;
     }
-}
+
+    public interface OnMovieSelectedListener {
+        public void onMovieSelected(int index);
+    }
+
+    public void setListener(OnMovieSelectedListener listener) {
+        mListener = listener;
+    }
+
+
+} /* eoc */
+
+
